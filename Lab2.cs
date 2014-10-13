7ad9a6daafb034489cf20046a729693dd26220a9
James W. Kimani <jkimani2@gmail.com>
2014-10-13 09:57:19 -0400

    Added labs and assignments

---
+++ Lab2.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("MAIN MENU\n\n");
+
+                Console.WriteLine("Please Choose the option you would like to run: ");
+                Console.WriteLine("A: Enter A for factorial operations(in the range [1,10])");
+                Console.WriteLine("B: Enter B for Person class");
+                Console.WriteLine("X: Enter X to exit and Close this window.");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                char myChar = char.ToUpper(key.KeyChar);
+
+                if (myChar == 'X')
+                {
+                    Console.WriteLine("\n\n\tGood-Bye.");
+                    break;
+                }
+                if (myChar == 'A')
+                {
+                    FactorialOps();
+                    break;
+                }
+                if (myChar == 'B')
+                {
+                    PersonClass();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\n\nInvalid Entry. Please try again...");
+                    break;
+                }
+
+            }
+        }
+
+        public static void PersonClass()
+        {
+            Console.Clear();
+            Console.WriteLine("PERSON CLASS");
+
+            Person person = new Person();
+            Console.Write("Enter the person's first name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter the person's last name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter the email: ");
+            person.Email = Console.ReadLine();
+
+            Console.WriteLine("\nThe person's data is: ");
+            Console.WriteLine(person);
+
+        }
+
+        public static void FactorialOps()
+        {
+            Console.Clear();
+            Console.WriteLine("generates the product of all the numbers in the range [1, 10]  ");
+
+            int[] myRange = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int result = 1;
+            for (int i = 1; i <= 9; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("1* 2* 3* 4* 5* 6* 7* 8* 9* 10 = " + result);
+
+        }
+
+    }
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format(this.FirstName +
+                " " + this.LastName +
+                " " + this.email + " ");
+        }
+
+
+    }
+
+}
+
---
+++ Lab8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab8
+{
+    class Program
+    {
+        
+        static void print_r(string s)
+        {
+            if (s.Length > 0)
+                print_r(s.Substring(1,s.Length - 1));
+            else
+                return;
+            Console.Write(s[0]);
+        }
+        static void print_rn(string s)
+        {
+
+            for (int i = s.Length - 1; i >= 0; i-- )
+            {
+                Console.Write(s[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Enter a string to be reversed by recursion: ");
+            String s = Console.ReadLine();
+            Console.Write(s + " reversed by recursion is: ");
+            print_r(s);
+            Console.WriteLine();
+            Console.Write("Enter a string to be reversed by iteration: ");
+            String a = Console.ReadLine();
+            Console.Write(a + " reversed by iteration is: ");
+            print_rn(a);
+            Console.WriteLine();
+
+        }
+    }
+}
